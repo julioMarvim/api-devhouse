e9e1c3018b94e46247dc505ccb03c6f8fbbe9bcd
julioMarvim <juliomarvim@yahoo.com.br>
2023-05-08 18:03:06 -0300

    feat: connecta no mongoDb, configura dotenv.

--- src/app.js
+++ src/app.js
@@ -1,10 +1,17 @@
 import express from 'express';
+import mongoose from 'mongoose';
 import routes from './routes.js';
+import dotenv from 'dotenv';
+dotenv.config();
 
 class App{
   //construtor é chamado sempre que a classe for instanciada. 
   constructor(){
     this.server = express();
+    const CONNECTION_STRING = process.env.DB_CONNECTION_STRING
+    mongoose.connect(CONNECTION_STRING);
+
+    this.middlewares();
     this.routes();
 
   }
