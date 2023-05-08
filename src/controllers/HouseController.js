1b365e6a3fdfcef1b72ebe8465a4c7f77ee324cd
julioMarvim <juliomarvim@yahoo.com.br>
2023-05-08 19:05:58 -0300

    feat: cria house

---
+++ src/controllers/HouseController.js
@@ -0,0 +1,9 @@
+class HouseController{
+  async store(req, res){
+    console.log(req.body);
+    console.log(req.file);
+    return res.json({ok: true});
+  }
+}
+
+export default new HouseController();
