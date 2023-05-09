4474c612c0f75830e9e8e1903c665dc3fb9821f2
julioMarvim <juliomarvim@yahoo.com.br>
2023-05-09 11:19:49 -0300

    feat: cria rota de update

--- src/controllers/HouseController.js
+++ src/controllers/HouseController.js
@@ -1,4 +1,5 @@
 import House from '../models/House.js'
+import User from '../models/User.js';
 
 class HouseController{
 
@@ -23,6 +24,31 @@ class HouseController{
     });
     
     return res.json(house);
+  }
+
+  async update(req, res){
+    const {filename} = req.file;
+    const {house_id} = req.params;
+    const {description, price, location, status} = req.body;
+    const {user_id} = req.headers;
+
+    const user = await User.findById(user_id);
+    const house = await House.findById(house_id);
+    
+    if(String(user._id) !== String(house.user)){
+      return res.status(401).json({error: 'Não autorizado.'});
+    }
+
+    await House.updateOne({_id: house_id}, {
+      user: user_id,
+      thumbnail: filename,
+      description,
+      price,
+      location,
+      status,
+    });
+
+    return res.send();
   }
 }
 
