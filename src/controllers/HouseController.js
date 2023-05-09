ab3e1edcd611a8c74dcc7a1d2a182c55458f254a
julioMarvim <juliomarvim@yahoo.com.br>
2023-05-09 10:07:19 -0300

    feat: finalizando a criação da casa

--- src/controllers/HouseController.js
+++ src/controllers/HouseController.js
@@ -1,8 +1,21 @@
+import House from '../models/House.js'
+
 class HouseController{
   async store(req, res){
-    console.log(req.body);
-    console.log(req.file);
-    return res.json({ok: true});
+    const {filename} = req.file;
+    const {description, price, location, status} = req.body;
+    const {user_id} = req.headers;
+
+    const house = await House.create({
+      user: user_id,
+      thumbnail: filename,
+      description,
+      price,
+      location,
+      status,
+    });
+    
+    return res.json(house);
   }
 }
 
